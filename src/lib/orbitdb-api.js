c64d678e7eeeaf2251e7786df116bc3c19db3c75
phillmac <phillmac@users.noreply.github.com>
2019-05-24 09:13:08 +1000

    Use map instead of plain object

--- src/lib/orbitdb-api.js
+++ src/lib/orbitdb-api.js
@@ -239,7 +239,7 @@ class OrbitdbAPI {
                 handler: dbMiddleware( async (db, request, h) => {
                     let eventname = request.params.eventname
 
-                    let event_map = {
+                    let event_map = Map(Object.entries({
                         'replicated': (address) =>
                             h.event({event:'replicated', data: {address:address}}),
                         'replicate.progress': (address, hash, entry, progress, have) =>
@@ -254,7 +254,7 @@ class OrbitdbAPI {
                                 h.event({event:'write', data: {dbname:dbname, hash:hash, entry:entry}}),
                         'closed': (dbname) =>
                                 h.event({event:'closed', data: {dbname:dbname}})
-                    }
+                    }));
 
                     event_callback = event_map.get(eventname)
                     if(event_callback){
