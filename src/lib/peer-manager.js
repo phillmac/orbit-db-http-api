68de42265560d4768847eff9109094513dac0e05
phillmac <phillmac@users.noreply.github.com>
2019-10-19 18:09:59 +1100

    Fix debug message

--- src/lib/peer-manager.js
+++ src/lib/peer-manager.js
@@ -220,7 +220,7 @@ class PeerManager {
             result.on('end', () => resolve(peers))
             result.on('data', chunk => {
               if (chunk.Type === 4) {
-                logger.debug(`Found peer from DHT: ${peer}`)
+                logger.debug(`Found peers from DHT: ${chunk}`)
                 peers = peers.concat(chunk.Responses.map(r => createPeerInfo(r)))
               }
             })
