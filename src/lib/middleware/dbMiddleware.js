631900c1650c893f040baa20b8d7ffe318924866
phillmac <phillmac@users.noreply.github.com>
2019-11-30 12:21:39 +1100

    Cleanup

--- src/lib/middleware/dbMiddleware.js
+++ src/lib/middleware/dbMiddleware.js
@@ -2,7 +2,7 @@ module.exports = function (dbMan) {
   return fn =>
     async (request, h) => {
       const db = await dbMan.get(request.params.dbname)
-      return Promise.resolve((fn(db, request, h)))
+      return Promise.resolve(fn(db, request, h))
         .catch((err) => { throw err })
     }
 }
